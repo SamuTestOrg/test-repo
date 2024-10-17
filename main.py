86d02a14d01f96fd34db84364393260f74944f63
Samuel Martín Cantalejo <samuel.cantalejo@datadoghq.com>
2024-10-17 10:25:13 +0200

    Add third endpoint

--- main.py
+++ main.py
@@ -18,5 +18,13 @@ def test_sources_7(something):
 
     return "foo"
 
+# curl -X GET "http://localhost:5000/tainted9/touch%20HELLO"
+@app.route("/tainted9/<something>")
+def test_sources_7(something):
+
+    os.system(request.remote_addr)
+
+    return "foo"
+
 if __name__ == "__main__":
 	app.run(debug=True)
